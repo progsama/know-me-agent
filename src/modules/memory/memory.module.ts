6b93ab0c76c154d35f7130d4152fd3665a77ec76
progsama <imtiaznasif@gmail.com>
2026-04-19 14:17:05 -0700

    phase 6 completed

--- src/modules/memory/memory.module.ts
+++ src/modules/memory/memory.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { EmbeddingService } from './embedding.service';
 import { SemanticSearchService } from './semantic-search.service';
+import { ContextAssemblyService } from './context-assembly.service';
 import { DatabaseModule } from '../../database/database.module';
+import { EntitiesModule } from '../entities/entities.module';
 
 @Module({
-  imports: [DatabaseModule],
-  providers: [EmbeddingService, SemanticSearchService],
-  exports: [EmbeddingService, SemanticSearchService],
+  imports: [DatabaseModule, EntitiesModule],
+  providers: [
+    EmbeddingService,
+    SemanticSearchService,
+    ContextAssemblyService,
+  ],
+  exports: [
+    EmbeddingService,
+    SemanticSearchService,
+    ContextAssemblyService,
+  ],
 })
 export class MemoryModule {}
